0dc6098a443d761591fdcef239a26f0da057612b
onurozkinaci <onurozkinaci@gmail.com>
2021-04-20 01:10:22 +0300

    new updates
    new updates

---
+++ User.java
@@ -0,0 +1,23 @@
+
+public class User 
+{
+  private String userName;
+  private String password;
+  public User(String username,String password) 
+  {
+	setUserName(username);
+	setPassword(password);
+  }
+  public String getUserName() {
+	return userName;
+  }
+  public void setUserName(String userName) {
+	this.userName = userName;
+  }
+  public String getPassword() {
+	return password;
+ }
+  public void setPassword(String password) {
+	this.password = password;
+  }
+}
